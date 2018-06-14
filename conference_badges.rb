130438b06f3296cd20f82efc7140f4b876d4bf51
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 06:43:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,13 @@ def batch_badge_creator(array)
   badge_array
 end
 def assign_rooms(speakers)
-  
+  count = 1 
+  rooms = []
+  speakers.each do |speaker|
+    rooms.push("Hello #{speaker}! You'll be assigned room #{count}")
+    count += 1 
+  end
+  rooms
 end
 def printer
 end
